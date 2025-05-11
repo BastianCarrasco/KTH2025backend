1fb41978ab9985ab0ce270763cc5a372422c7c02
BastianCarrasco <88857266+BastianCarrasco@users.noreply.github.com>
2025-05-10 21:14:50 -0400

    fix alternativas

--- backend/queries/alternativas.ts
+++ backend/queries/alternativas.ts
@@ -1,6 +1,6 @@
 import { pool } from "../db";
 
 export const getAllAlternativas = async () => {
-  const result = await pool.query("SELECT * FROM alternaticas");
+  const result = await pool.query("SELECT * FROM alternativas");
   return result.rows;
 };
