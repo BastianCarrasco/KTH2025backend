ac60f55906366bf006c23a6222dc33431e203772
BastianCarrasco <88857266+BastianCarrasco@users.noreply.github.com>
2025-05-18 14:56:38 -0400

    getproyectos mejorados

--- backend/queries/proyectos.ts
+++ backend/queries/proyectos.ts
@@ -18,6 +18,30 @@ export const insertProyecto = async ({
 }
 
 export const getProyectos = async () => {
-  const result = await pool.query("SELECT * FROM proyecto");
+  const query = `
+    SELECT
+      P.ID_PROYECTO,
+      P.NOMBRE,
+      P.MONTO,
+      P.FECHA_POSTULACION,
+      P.COMENTARIOS,
+      U.NOMBRE AS "UA",
+      CON.NOMBRE AS "Convocatoria",
+      TC.NOMBRE AS "Tipo_Convo",
+      IC.NOMBRE AS "Institucion_convocatoria",
+      AC.NOMBRE AS "Academico",
+      PA.JEFE
+    FROM
+      PROYECTO AS P
+      LEFT JOIN CONVOCATORIA AS CON ON CON.ID_CONVOCATORIA = P.ID_CONVOCATORIA
+      LEFT JOIN UNIDADACADEMICA AS U ON U.ID_UNIDAD = P.UNIDAD
+      LEFT JOIN TIPO_CONVO AS TC ON TC.ID = CON.TIPO
+      LEFT JOIN INST_CONVO AS IC ON IC.ID = CON.INSTITUCION
+      LEFT JOIN proyectoacademico AS PA ON PA.id_proyecto = P.ID_PROYECTO
+      LEFT JOIN academico AS AC ON AC.id_academico = PA.id_academico
+    ORDER BY P.ID_PROYECTO
+  `;
+  
+  const result = await pool.query(query);
   return result.rows;
 };
