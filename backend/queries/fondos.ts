4be928091ac75f1672a2f23fcb0a5a209bf5ff4c
BastianCarrasco <88857266+BastianCarrasco@users.noreply.github.com>
2025-05-20 13:08:57 -0400

    query fondos mejorada

--- backend/queries/fondos.ts
+++ backend/queries/fondos.ts
@@ -1,6 +1,13 @@
 import { pool } from "../db";
 
 export const getfondos = async () => {
-  const result = await pool.query("SELECT * FROM fondos");
+    const result = await pool.query(`
+    SELECT f.*, tc.* 
+    FROM fondos f
+    JOIN tipo_convo tc ON f.id = tc.id
+    ORDER BY f.id
+  `);
   return result.rows;
 };
+
+
