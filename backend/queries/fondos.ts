ef6c6c93e8eb46823db54179ae47640fed8d94e2
BastianCarrasco <88857266+BastianCarrasco@users.noreply.github.com>
2025-05-20 13:31:25 -0400

    fix fondos nombres

--- backend/queries/fondos.ts
+++ backend/queries/fondos.ts
@@ -2,7 +2,7 @@ import { pool } from "../db";
 
 export const getfondos = async () => {
     const result = await pool.query(`
-    SELECT f.*, tc.* 
+    SELECT f.*, tc.nombre as "tipo de fondo" 
     FROM fondos f
     JOIN tipo_convo tc ON f.tipo = tc.id
     ORDER BY f.id
