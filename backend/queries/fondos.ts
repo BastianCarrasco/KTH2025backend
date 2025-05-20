4062e84c2b6fe7dd766067cb7da53a0f87126f4b
BastianCarrasco <88857266+BastianCarrasco@users.noreply.github.com>
2025-05-20 13:26:18 -0400

    error de query fondos arreglado

--- backend/queries/fondos.ts
+++ backend/queries/fondos.ts
@@ -4,7 +4,7 @@ export const getfondos = async () => {
     const result = await pool.query(`
     SELECT f.*, tc.* 
     FROM fondos f
-    JOIN tipo_convo tc ON f.id = tc.id
+    JOIN tipo_convo tc ON f.tipo = tc.id
     ORDER BY f.id
   `);
   return result.rows;
