3ac9ea0100b457dee68893e25bbb50b5ec3810e4
BastianCarrasco <88857266+BastianCarrasco@users.noreply.github.com>
2025-06-04 21:34:24 -0400

    orden de nombre

--- backend/queries/academicos.ts
+++ backend/queries/academicos.ts
@@ -1,6 +1,6 @@
 import { pool } from "../db";
 
 export const getAcademicos = async () => {
-  const result = await pool.query("SELECT * FROM academico");
+  const result = await pool.query("SELECT * FROM academico order by (nombre)");
   return result.rows;
 };
