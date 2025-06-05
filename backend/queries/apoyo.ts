de7dc283806fb99679e09c4761d5241922e1ef3e
BastianCarrasco <88857266+BastianCarrasco@users.noreply.github.com>
2025-06-04 21:55:13 -0400

    ordenar apoyo

--- backend/queries/apoyo.ts
+++ backend/queries/apoyo.ts
@@ -2,7 +2,7 @@ import { pool } from "../db";
 
 export const getApoyo = async () => {
   const result = await pool.query(
-    "select apoyo.id_apoyo,apoyo.detalle,tipo_apoyo.tipo from apoyo join tipo_apoyo on apoyo.tipo=tipo_apoyo.id_tipo_apoyo"
+    "select apoyo.id_apoyo,apoyo.detalle,tipo_apoyo.tipo from apoyo join tipo_apoyo on apoyo.tipo=tipo_apoyo.id_tipo_apoyo order by TIPO_APOYO.TIPO"
   );
   return result.rows;
 };
